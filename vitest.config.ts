60b76394f6ded56878f9b45b1c3d2b131caf6a88
Darshil Shah <darshils99@gmail.com>
2026-09-05 17:15:39 -0500

    Add files via upload

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'jsdom',
-    exclude: ['**/node_modules/**', '**/dist/**', 'server/**'],
+    exclude: ['**/node_modules/**', '**/dist/**', 'server/**', 'e2e/**'],
     globals: true,
   },
 });
